1ffd566205430a71cad1e5bcda17a58ce55a3256
lad1337 <lad1337@gmail.com>
2025-06-18 00:16:17 +0200

    integrate split def vertical with goto-preview

--- config/nvim/lua/lad1337/functions.lua
+++ config/nvim/lua/lad1337/functions.lua
@@ -8,7 +8,10 @@ function split_definition_vertical()
       vim.notify('No definition to open.', vim.log.levels.WARN)
       return
     end
-
+    if vim.api.nvim_win_get_config(0).zindex ~= nil then -- are we in a floating window aka goto-preview
+      vim.api.nvim_buf_del_keymap(0, 'n', 'q') -- remove the q to close
+      vim.api.nvim_win_close(0, false) -- do not force
+    end
     local command = 'vsplit ' .. vim.uri_to_fname(result[1].uri)
     local line = 'call cursor(' .. (result[1].range.start.line + 1) .. ',' .. (result[1].range.start.character + 1) .. ')'
     vim.cmd(command)
